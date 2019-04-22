d4668bdffc94c96737f69c2925105c7b1ca9d3a5
ak7 <abdelrhman.khater111@gmail.com>
2019-04-22 11:54:43 +0200

    first commit

---
+++ routes/gigs.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../config/database');
+const Gig = require('../models/Gig');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+router.get('/', (req, res) => 
+    Gig.findAll()
+    .then(gigs => {
+        console.log(gigs);
+        res.render('gigs', {
+            gigs
+        });
+    })
+    .catch(
+        err => {
+            console.log(err)
+        }
+    )
+);
+router.get('/add', (req, res) => res.render('add'));
+router.post('/add', (req, res) => {
+    let{title, technologies, budget, description, contact_email} = req.body;
+    let errors = [];
+    if (!title)
+    {
+        errors.push({text: 'Please add a title'});
+    }
+    if (!technologies)
+    {
+        errors.push({text: 'Please add a technologies'});
+    }
+    if (!budget)
+    {
+        errors.push({text: 'Please add a budget'});
+    }
+    if (!description)
+    {
+        errors.push({text: 'Please add a description'});
+    }
+    if (!contact_email)
+    {
+        errors.push({text: 'Please add a contact_email'});
+    }
+    if (errors.length > 0)
+    {
+        res.render('add',
+        {
+            errors,
+            title,
+            technologies,
+            budget,
+            description,
+            contact_email
+        });
+    }
+    else
+    {
+        budget = `$${budget}`;
+        technologies = technologies.toLowerCase().replace(/, /g, ',');
+        Gig.create({
+            title,
+            technologies,
+            description,
+            budget,
+            contact_email
+        })
+        .then(gig => res.redirect('/gigs'))
+        .catch(err => console.log(err));
+    }
+});
+router.get('/search', (req, res) =>{
+    let {term} = req.query;
+    term = term.toLowerCase();
+    Gig.findAll({where : { technologies: {[Op.like]: '%' + term + '%' }}})
+    .then(gigs => res.render('gigs', {gigs}))
+    .catch(err => console.log(err));
+});
+module.exports = router;
